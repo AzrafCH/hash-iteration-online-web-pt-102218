15f452db08b22aeef9dbaaece09f6a22fe42e737
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-31 07:26:41 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,13 +7,11 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
-  winners = ""
-  passengers.each do | suite, name |
-    if suite == :suite_a && name.start_with?("A")
-      winner = name 
-    end
-    winner
-  end 
-  
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ winner 
 end
